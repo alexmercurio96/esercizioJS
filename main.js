0b3844d2dfc3207aa452d65d490d584ec3d40315
alexmercurio96 <alexmercurio23@gmail.com>
2023-11-02 17:46:03 +0100

    eserciziocompletato

---
+++ main.js
@@ -0,0 +1,97 @@
+console.log (`ciao`)
+
+let nom = (`alex`);
+
+console.log (typeof nom)
+
+let a = 5;
+
+let b = 5;
+
+console.log (a+b);
+
+console.log (a*b);
+
+// ESERCIZIO 01
+
+let nome = `sono alex e ho`;
+
+let age = 27;
+
+let materia = `e sto studiando javascript`;
+
+let docente = `con il professor oak (valerio vacca)`;
+
+let somma =` ${nome} ${age} ${materia} ${docente}`;
+
+console.log  (somma)
+
+// ESERCIZIO 02
+
+let f = 1;
+
+let g = 2;
+
+let h = 3;
+
+let i = 4;
+
+let l = 5;
+
+let m = 6;
+
+let addizione = f + g + h + i + l + m;
+
+console.log  (addizione)
+ 
+let media = addizione / 5;
+
+console.log (media)
+ 
+// ESERCIZIO 03
+
+let anno_corrente = 2023;
+
+let anno_nascita = 1996;
+
+let sottrazione = anno_corrente - anno_nascita;
+
+console.log (sottrazione)
+
+let babi = 100;
+
+let formula = babi - sottrazione;
+ 
+console.log (formula)
+
+// ESERZIZIO 04 
+
+
+let gatti = 44;
+
+let gattixfila = 6;
+
+let file = gatti / gattixfila;
+
+console.log (file);
+
+let file_intere = Math.floor(file);
+
+console.log (file_intere)
+
+let gatti_ultima_fila = gatti - file_intere * gattixfila;
+
+console.log (gatti_ultima_fila) ;
+
+let ultima_fila = gattixfila - gatti_ultima_fila;
+
+console.log (ultima_fila);
+
+let frase1 = `ci sono ${file_intere} file di gatti e ne mancano ${ultima_fila} per una nuova fila, con un avanzo di ${gatti_ultima_fila}`;
+
+console.log(frase1) 
+
+
+
+
+
